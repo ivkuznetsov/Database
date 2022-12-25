65975585515332b70ffabd973819a10ba82b2256
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2022-12-25 18:33:07 +0100

    fixed codable transformer

--- Sources/Database/CodableTransformer.swift
+++ Sources/Database/CodableTransformer.swift
@@ -19,6 +19,8 @@ public class CodableTransformer: ValueTransformer {
                 self = .string(value)
             } else if let value = value as? NSNumber {
                 self = .number(try! JSONSerialization.data(withJSONObject: [value]))
+            } else if let value = value as? [String] {
+                self = .array(value.compactMap { .string($0) })
             } else if let value = value as? [Encodable] {
                 self = .array(value.compactMap { Value(value: $0) })
             } else if let value = value as? AnyObject & Encodable {
